f613270d90512380bbf7735905689d4be49d9632
jdrisc <80981725+jdrisc@users.noreply.github.com>
2023-03-11 12:23:33 +0000

    Update hopf.js
    added imports at start

--- docs/js/hopf.js
+++ docs/js/hopf.js
@@ -1,3 +1,8 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
